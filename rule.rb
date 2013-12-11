e652d01e3b7134a46920b419c0eac1b1045e5a8c
Wes Williams <wes.williamz@gmail.com>
2013-12-11 22:49:22 +0800

    refactoring

--- rule.rb
+++ rule.rb
@@ -15,7 +15,8 @@ module GameOfLife
 
   def rules
     @rules = [
-        Rule.new(GameOfLife::ALIVE_CELL) { |neighbors, current_state| ( current_state == GameOfLife::ALIVE_CELL && neighbors.number_alive == 2 ) },
+        Rule.new(GameOfLife::ALIVE_CELL) { |neighbors, current_state|
+          ( current_state == GameOfLife::ALIVE_CELL && neighbors.number_alive == 2 ) },
         Rule.new(GameOfLife::ALIVE_CELL) { |neighbors,| ( neighbors.number_alive == 3 ) },
         Rule.new(GameOfLife::DEAD_CELL) { |neighbors| neighbors.number_alive < 2 },
         Rule.new(GameOfLife::DEAD_CELL) { |neighbors| neighbors.number_alive > 3 }
