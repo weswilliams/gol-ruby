a75a490479869062ad44bdac99e7e814c891f0dd
Wes Williams <wes.williamz@gmail.com>
2013-12-11 21:44:06 +0800

    refactoring

--- neighbors.rb
+++ neighbors.rb
@@ -5,9 +5,9 @@ module GameOfLife
     def initialize(cells = [])
       @cells = cells
       @rules = [
-          DeadRule.new { |neighbors| neighbors.number_alive < 2 },
           AliveRule.new { |neighbors, current_state| ( current_state == GameOfLife::ALIVE_CELL && neighbors.number_alive == 2 ) },
           AliveRule.new { |neighbors,| ( neighbors.number_alive == 3 ) },
+          DeadRule.new { |neighbors| neighbors.number_alive < 2 },
           DeadRule.new { |neighbors| neighbors.number_alive > 3 }
       ]
     end
