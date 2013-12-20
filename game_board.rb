8bb75a2c4d0b195e5b539922bdd43ac9021c0288
Wes Williams <wes.williamz@gmail.com>
2013-12-20 17:24:18 +0800

    convert config to cells

--- game_board.rb
+++ game_board.rb
@@ -1,5 +1,18 @@
-class GameBoard
-  def rows(board_config)
-    board_config.split("\n")
+module GameOfLife
+
+  class GameBoard
+    def rows(board_config)
+      board_config.split("\n")
+    end
+
+    def columns_for(row_cells)
+      row_cells.chars.collect { |cell| Cell.new(state_for(cell)) }
+    end
+
+    def state_for(cell_representation)
+      return GameOfLife::ALIVE_CELL if "X" == cell_representation.upcase
+      GameOfLife::DEAD_CELL
+    end
   end
+
 end
--- game_board_spec.rb
+++ game_board_spec.rb
@@ -4,11 +4,16 @@ require_relative 'game_board'
 describe 'initial state' do
 
   before do
-    @board = GameBoard.new
+    @board = GameOfLife::GameBoard.new
+  end
+
+  describe 'of columns' do
+    it 'should have live cells' do
+      @board.columns_for("x")[0].is_alive.should == true
+    end
   end
 
   describe 'of rows' do
-
     it 'should create live cell rows' do
       @board.rows("xxx\nxxx\nxxx\n").size.should == 3
     end
