8c27620012daa576b43d8f0ff59a9cb13cd645e9
Wes Williams <wes.williamz@gmail.com>
2014-01-05 19:01:47 +0800

    glider gun

--- game_runner.rb
+++ game_runner.rb
@@ -3,7 +3,7 @@ require_relative 'game_board'
 game = GameOfLife::GameBoard.new File.open((ARGV.first || 'spinner') + '.txt', 'r').each_line.reduce('', :+)
 
 (0..100).each do
-  sleep 0.2
+  sleep 0.3
   system 'clear'
   puts game.next_life.to_s_size(0,20)
 end
