db43f4d51e5bd1ee695987c6795d82a8682f91df
Wes Williams <wes.williamz@gmail.com>
2014-01-05 18:12:45 +0800

    making runner show one screens worth of the world

--- game_runner.rb
+++ game_runner.rb
@@ -2,8 +2,8 @@ require_relative 'game_board'
 
 game = GameOfLife::GameBoard.new "          \n  X       \n   X      \n XXX      \n          \n          \n          \n"
 
-(0..20).each do
-  sleep 0.5
+(0..100).each do
+  sleep 0.2
   system 'clear'
-  puts game.next_life
+  puts game.next_life.to_s_size(0,20)
 end
