4ddb4280cb2ab0354549574a4e5c473e454a8673
Wes Williams <wes.williamz@gmail.com>
2014-01-05 20:34:45 +0800

    added die-hard and found defect

--- game_runner.rb
+++ game_runner.rb
@@ -1,9 +1,12 @@
 require_relative 'game_board'
 
-game = GameOfLife::GameBoard.new File.open((ARGV.first || 'spinner') + '.txt', 'r').each_line.reduce('', :+)
+game = GameOfLife::GameBoard.new File.open((ARGV[0] || 'spinner') + '.txt', 'r').each_line.reduce('', :+)
+board_size = ARGV[1] || 20
+number_lives = ARGV[2] || 1000
 
-(0..10000).each do
-  sleep 0.5
+(0..number_lives.to_i).each do |cnt|
+  sleep 0.3
   system 'clear'
-  puts game.next_life.to_s_size(0,20)
+  puts game.next_life.to_s_size(0,board_size.to_i)
+  puts "life \##{cnt}"
 end
