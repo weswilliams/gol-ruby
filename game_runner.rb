cbc45a2f204c352fd740d268d0ac55f07b52999b
Wes Williams <wes.williamz@gmail.com>
2014-01-05 19:54:30 +0800

    new pattern and longer life states

--- game_runner.rb
+++ game_runner.rb
@@ -2,8 +2,8 @@ require_relative 'game_board'
 
 game = GameOfLife::GameBoard.new File.open((ARGV.first || 'spinner') + '.txt', 'r').each_line.reduce('', :+)
 
-(0..100).each do
-  sleep 0.3
+(0..10000).each do
+  sleep 0.5
   system 'clear'
   puts game.next_life.to_s_size(0,20)
 end
