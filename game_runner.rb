666cc6df78b3b2f87ad7be50b8f5e04c6e52f0f8
Wes Williams <wes.williamz@gmail.com>
2014-01-10 14:07:10 +0800

    reduce sleep time as next_life gen increases

--- game_runner.rb
+++ game_runner.rb
@@ -3,11 +3,13 @@ require_relative 'game_board'
 game = GameOfLife::GameBoard.new File.open((ARGV[0] || 'spinner') + '.txt', 'r').each_line.reduce('', :+)
 board_size = ARGV[1] || 20
 number_lives = ARGV[2] || 1000
+@last_gen_time = 0
 
 (0..number_lives.to_i).each do |cnt|
-  sleep 0.2
+  sleep [0, (0.2 - @last_gen_time)].max
   system 'clear'
   start_gen = Time.now
   puts game.next_life.to_s_size(-5,board_size.to_i)
-  puts "life \##{cnt} created in #{Time.now - start_gen}"
+  @last_gen_time = Time.now - start_gen
+  puts "life \##{cnt} created in #{@last_gen_time}"
 end
